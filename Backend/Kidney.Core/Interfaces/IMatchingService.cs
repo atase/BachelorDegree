15141ce63c5d47b995ab8c9cc71889bd0e14d4ea
petrunastasa-calvi <petru.nastasa@tss-yonder.com>
2021-12-18 12:59:26 +0200

    Deleted old backend solutions.

--- Backend/Kidney.Core/Interfaces/IMatchingService.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Kidney.Core.Services
-{
-    public interface IMatchingService
-    {
-        /*public MatchingDataDTO<GiverDTO, ReceiverDTO> Matching();
-        public bool Compatible(Giver giver, Receiver receiver);*/
-    }
-}
